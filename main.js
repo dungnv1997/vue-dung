36561897332494fa0936608526750eb6f9590516
dungnv1997 <80682820+dungnv1997@users.noreply.github.com>
2021-03-16 17:25:29 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,183 +1,215 @@
-new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue.js!'
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
     }
-  })
-  var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'Bạn đã mở trang này vào ' + new Date().toLocaleString()
-    }
-  })
-  app2.message = 'Cái gì đó khác';
-  var app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  })
-  app3.seen=false;
-  var app4 = new Vue({
-    el: '#app-4',
-    data: {
-      todos: [
-        { text: 'Học JavaScript' },
-        { text: 'Học Vue' },
-        { text: 'Xây dựng cái gì đó hay ho' }
-      ]
-    }
-  })
-  app4.todos.push({text:'nghỉ ngơi'});
-  var app5 = new Vue({
-      el:"#app-5",
-      data:{message:'quay lại khách chờ sông lặng gió'},
-      methods:{
-        reverMessage: function(){
-            this.message=this.message.split(' ').reverse().join(' ')
-      }
-  }
-})
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-      message: 'Hãy sửa thông điệp này'
-    }
-  })
-  Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-  })
-  
-  var app7 = new Vue({
-    el: '#app-7',
-    data: {
-      groceryList: [
-        { id: 0, text: 'Cà pháo' },
-        { id: 1, text: 'Mắm tôm' },
-        { id: 2, text: 'Miễn ăn được là được' }
-      ]
-    }
-  })
-  var vm = new Vue({
-    el: '#example',
-    data: {
-      message: 'người đông bến đợi thuyền xuôi ngược'
-    },
-    computed: {
-      // một computed getter
-      reversedMessage: function () {
-        // `this` trỏ tới đối tượng vm
-        return this.message.split(' ').reverse().join(' ')
-      }
-    }
-  })
-  vm.message = 'xa ngân tiếng hát đàn trầm bổng'
-  var vm = new Vue({
-    el: '#demo',
-    data: {
-      firstName: 'Trần',
-      lastName: 'Lập',
-      fullName: 'Trần Lập'
-    },
-    watch: {
-      firstName: function (val) {
-        this.fullName = val + ' ' + this.lastName
-      },
-      lastName: function (val) {
-        this.fullName = this.firstName + ' ' + val
-      }
-    }
-  })
- 
-var watchExampleVM = new Vue({
-  el: '#watch-example',
-  data: {
-    question: '',
-    answer: 'Không thể trả lời nếu bạn chưa đặt câu hỏi!'
   },
-  watch: {
-    // bất cứ lúc nào câu hỏi thay đổi, hàm bên dưới sẽ chạy
-    question: function (newQuestion, oldQuestion) {
-      this.answer = 'Đang chờ bạn đặt xong câu hỏi...'
-      this.getAnswer()
+  template: '<button v-on:click="count++">Bạn đã bấm {{ count }} lần.</button>'
+})
+new Vue({ el: '#components-demo' })
+
+Vue.component('button-read', {
+  data: function () {
+    return {
+      count: 0
     }
+  },
+  template: '<button v-on:click="count++">Bạn đã bấm {{ count }} lần.</button>'
+})
+new Vue({ el: '#components-demo1' })
+Vue.component('blog-post',{
+  props:['title'],
+  template:'<h3>{{title}}</h3>'
+})
+new Vue({ el: '#components-demo2' })
+Vue.component('child', {
+  props: ['message'],
+  template: '<h1>{{ message }}</h1>',
+  
+});
+var app = new Vue({
+  el: '#app',
+});
+Vue.component('child', {
+  props: ['mySay'],
+  template: '<h1>{{ mySay }}</h1>',
+
+});
+var app = new Vue({
+  el: '#app2',
+  data: {
+      message : null
+  }
+});
+Vue.component('child',{
+  template: '<button v-on:click="numberPlus">{{ count }}</button>',
+  data: function(){
+    return {count:0};
+  },
+  methods:{
+    numberPlus: function(){
+      this.count +=1;
+      this.$emit('counter')
+    }
+  }
+});
+var app = new Vue({
+  el:'#app3',
+  data:{
+    count:0
+  },
+  methods:{
+    countPlus: function(){
+      this.count +=1;
+    }
+  }
+});
+Vue.component('child', {
+  template: '<button>Click</button>'
+});
+var app = new Vue({
+  el: '#app4',
+  data: {
+      count: 0
   },
   methods: {
-    // _.debounce là một hàm do Lodash cung cấp
-    // Để tìm hiểu rõ hơn cách hoạt động của hàm này,
-    // bạn có thể truy cập: https://lodash.com/docs#debounce 
-    getAnswer: _.debounce(
-      function () {
-        if (this.question.indexOf('?') === -1) {
-          this.answer = 'Câu hỏi thì thường chứa một dấu "?" ;-)'
-          return
-        }
-        this.answer = 'Đang suy nghĩ...'
-        var vm = this
-        axios.get('https://yesno.wtf/api')
-          .then(function (response) {
-            vm.answer = _.capitalize(response.data.answer)
-          })
-          .catch(function (error) {
-            vm.answer = 'Lỗi! Không thể truy cập API. ' + error
-          })
-      },
-      // Đây là thời gian (đơn vị mili giây) chúng ta đợi người dùng dừng gõ.
-      500
-    )
+      countPlus: function ()
+      {
+          this.count += 1;
+      }
   }
-})
-new Vue({
-    el: '#v-for-object',
-    data: {
-      object: {
-        // tất nhiên chúng ta đều biết ông Bành Tổ không phải
-        // họ Bành tên Tổ, nhưng đây chỉ là một ví dụ…
-        'họ': 'Bành',
-        'tên': 'Tổ',
-        'tuổi': 800
+});
+var demo = new Vue({
+
+  // A DOM element to mount our view model.
+  el: '#main',
+
+  // This is the model.
+  // Define properties and give them initial values.
+  data: {
+      active: 'home'
+  },
+
+  // Functions we will be using.
+  methods: {
+      makeActive: function(item){
+          // When a model is changed, the view will be automatically updated.
+          this.active = item;
       }
-    }
-  })
-  var example1 = new Vue({
-    el: '#example-1',
-    data: {
-      counter: 0
-    }
-  })
-  var example2 = new Vue({
-    el: '#example-2',
-    data: {
-      name: 'Vue.js'
-    },
-    // định nghĩa phương thức trong object `methods`
-    methods: {
-      greet: function (event) {
-        // bên trong một phương thức, `this` trỏ đến đối tượng Vue
-        alert('Xin chào ' + this.name + '!')
-        // `event` là sự kiện DOM native
-        if (event) {
-          alert(event.target.tagName)
-        }
+  }
+});
+Vue.filter('currency',function(value){
+  return '$' + value.toFixed(2);
+});
+// Define a custom filter called "currency". 
+Vue.filter('currency', function (value) {
+  return '$' + value.toFixed(2);
+});
+
+var demo = new Vue({
+  el: '#main1',
+  data: {
+      // Define the model properties. The view will loop
+      // through the services array and genreate a li
+      // element for every one of its items.
+      services: [
+          {
+              name: 'Web Development',
+              price: 300,
+              active:true
+          },{
+              name: 'Design',
+              price: 400,
+              active:false
+          },{
+              name: 'Integration',
+              price: 250,
+              active:false
+          },{
+              name: 'Training',
+              price: 220,
+              active:false
+          }
+      ]
+  },
+  methods: {
+      toggleActive: function(s){
+          s.active = !s.active;
+      },
+      total: function(){
+
+          var total = 0;
+
+          this.services.forEach(function(s){
+              if (s.active){
+                  total+= s.price;
+              }
+          });
+
+         return total;
       }
-    }
-  })
-  
-  // bạn cũng có thể gọi phương thức từ JavaScript
-  example2.greet() 
-  new Vue({
-    el: '#example-3',
-    methods: {
-      say: function (message) {
-        alert(message)
+  }
+});
+
+var demo = new Vue({
+  el: '#main2',
+  data: {
+      searchString: "",
+
+
+      articles: [
+          {
+              "title": "What You Need To Know About CSS Variables",
+              "url": "https://tutorialzine.com/2016/03/what-you-need-to-know-about-css-variables/",
+              "image": "https://tutorialzine.com/media/2016/03/css-variables.jpg"
+          },
+          {
+              "title": "Freebie: 4 Great Looking Pricing Tables",
+              "url": "https://tutorialzine.com/2016/02/freebie-4-great-looking-pricing-tables/",
+              "image": "https://tutorialzine.com/media/2016/02/great-looking-pricing-tables.jpg"
+          },
+          {
+              "title": "20 Interesting JavaScript and CSS Libraries for February 2016",
+              "url": "https://tutorialzine.com/2016/02/20-interesting-javascript-and-css-libraries-for-february-2016/",
+              "image": "https://tutorialzine.com/media/2016/02/interesting-resources-february.jpg"
+          },
+          {
+              "title": "Quick Tip: The Easiest Way To Make Responsive Headers",
+              "url": "https://tutorialzine.com/2016/02/quick-tip-easiest-way-to-make-responsive-headers/",
+              "image": "https://tutorialzine.com/media/2016/02/quick-tip-responsive-headers.png"
+          },
+          {
+              "title": "Learn SQL In 20 Minutes",
+              "url": "https://tutorialzine.com/2016/01/learn-sql-in-20-minutes/",
+              "image": "https://tutorialzine.com/media/2016/01/learn-sql-20-minutes.png"
+          },
+          {
+              "title": "Creating Your First Desktop App With HTML, JS and Electron",
+              "url": "https://tutorialzine.com/2015/12/creating-your-first-desktop-app-with-html-js-and-electron/",
+              "image": "https://tutorialzine.com/media/2015/12/creating-your-first-desktop-app-with-electron.png"
+          }
+      ]
+  },
+  computed: {
+    
+      filteredArticles: function () {
+          var articles_array = this.articles,
+              searchString = this.searchString;
+
+          if(!searchString){
+              return articles_array;
+          }
+
+          searchString = searchString.trim().toLowerCase();
+
+          articles_array = articles_array.filter(function(item){
+              if(item.title.toLowerCase().indexOf(searchString) !== -1){
+                  return item;
+              }
+          })
+
+          // Return an array with the filtered data.
+          return articles_array;;
       }
-    }
-  })
-  new Vue({
-    el: '#example-3',
-    data: {
-      checkedNames: []
-    }
-  })
- 
+  }
+});
